[FIX] bug with linalg and wrong dimmensions

src/libeq/solver/solver.py
@@ -122,9 +122,9 @@ def solve_equilibrium_equations(
         initial_guess = np.full_like(total_concentration, 1e-12)
     else:
         initial_guess = np.atleast_2d(initial_guess)
-
-    damping_fn = outer_fixed_point(
-        **outer_fiexd_point_params,
+    breakpoint()
+    damping_fn = outer_fixed_point(    # this is too convoluted
+        **outer_fiexd_point_params,    # TODO refactor for readability
     )(pcf)
 
     nr_fn = outer_fixed_point(
@@ -150,7 +150,7 @@ def solve_equilibrium_equations(
         ),
         axis=1,
     )
-
+    breakpoint()
     # Apply Newton-Raphson iterations
     result, log_beta, log_ks = nr_fn(
         damped_guess,

tests/test_potentiometry.py
@@ -91,6 +91,7 @@ def test_first():
     )
     sd = SolverData(
         components = ['EDTA', 'Zn', 'H'],
+        charges = [ -4, 2, 1 ],
         stoichiometry = np.array([[1,0, 1],
                                   [1,0, 2],
                                   [1,0, 3],
@@ -105,7 +106,7 @@ def test_first():
                                   [1,1, 0],
                                   [1,1, 1],
                                   [1,1,-1],
-                                  [0,0,-1]], dtype=int),
+                                  [0,0,-1]], dtype=int).T,
         solid_stoichiometry = np.array([[]], dtype=int),
         log_beta = np.array([ 10.19, 
                               16.32, 
@@ -124,7 +125,7 @@ def test_first():
                               -13.78]),
         potentiometry_opts = pot
     )
-    breakpoint()
+    # breakpoint()
     result = PotentiometryOptimizer(sd)
     x, concs, final_log_beta, b_error, cor_matrix, cov_matrix, return_extra = result
     print(result)