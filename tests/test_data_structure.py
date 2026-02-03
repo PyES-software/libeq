0bba6570c46950ba0c5d36ba28004c62d1b5c65d
Salvador Blasco <salvador.blasco@protonmail.com>
2026-02-03 11:00:54 +0100

    [add] new test for range

--- tests/test_data_structure.py
+++ tests/test_data_structure.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pytest
 
-from libeq.data_structure import PotentiometryTitrationsParameters
+from libeq.data_structure import PotentiometryTitrationsParameters, SolverData
+
+@pytest.fixture
+def znedta():
+    return SolverData.load_from_pyes("tests/data/znedta.json")
 
 
 @pytest.fixture
@@ -19,9 +23,16 @@ def ptp1():
     )
 
 
-def test_pxrange(ptp1):
+def test_pxrange1(ptp1):
     for upx in ptp1.pX:
         assert 2.0 <= upx <= 3.0 or 7.0 <= upx <= 9.0
+
+
+def test_pxrange2(znedta):
+    tit0 = znedta.potentiometry_opts.titrations[0]
+    tit0.px_range=[[2.0, 3.0], [8.0, 11.0]]
+    for upx in tit0.pX:
+        assert 2.0 <= upx <= 3.0 or 8.0 <= upx <= 11.0
 
 
 def test_pxcombined(ptp1):
